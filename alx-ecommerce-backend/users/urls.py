77db85b00c50c363b29a312559101d6786783fe2
Peter Oduor <peteropondo28@gmail.com>
2026-01-29 15:55:08 +0300

    Create urls.py

---
+++ alx-ecommerce-backend/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import RegisterView
+
+urlpatterns = [
+    path("register/", RegisterView.as_view()),
+    path("token/", TokenObtainPairView.as_view()),
+]
